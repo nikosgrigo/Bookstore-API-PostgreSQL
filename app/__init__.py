756d76341a56412dbfe583f8e24714c5a8fa9505
nikosgrigo <iis21084@uom.edu.gr>
2023-12-14 17:21:40 +0200

    Change data loading from csv to tables

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-from app.general import import_data
 from app.models import db
 
 app = Flask(__name__)
@@ -18,13 +17,3 @@ db.init_app(app)
 
 from app import routes, models
 
-
-# Create the database tables - RUN this one time
-# try:
-#     with app.app_context():
-#         db.create_all()
-#         import_data(db)
-
-        
-# except Exception as e:
-#     print(f"An error occurred: {e}")
